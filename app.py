41e81e992e22154114297ca34e8a0cbf22af6a53
vipul1919 <vikvipul19@gmail.com>
2020-06-04 23:47:31 +0530

    second commit

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, session
-from flask_session import Session
+#from flask_session import Session
 
 app = Flask(__name__)
 
 #app.config["SESSION_PERMANENT"] = False
 #app.config["SESSION_TYPE"] == "filesystem"
-Session(app)
+#Session(app)
 
 notes = [1]
 
